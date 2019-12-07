6d12a9986f6770ff5d82deb57957bb08f2670f65
shelly <shellystarss@gmail.com>
2019-12-07 14:41:16 +0700

    profile

--- app/src/main/java/com/witim/FragmentProfile.java
+++ app/src/main/java/com/witim/FragmentProfile.java
@@ -1,13 +1,18 @@
 package com.witim;
 
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +20,11 @@ import android.view.ViewGroup;
  */
 public class FragmentProfile extends Fragment {
 
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Model> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     public FragmentProfile() {
         // Required empty public constructor
@@ -24,8 +34,17 @@ public class FragmentProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        models = new ArrayList<>();
+        models.add(new Model(R.drawable.portfolio));
+        models.add(new Model(R.drawable.portfolio));
+        models.add(new Model(R.drawable.portfolio));
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+
     }
 
 }
